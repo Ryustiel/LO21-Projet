b837547c5584cfb411851cf09e32dcd58f89b276
WAnoukh <anoukhman@gmail.com>
2023-05-10 16:53:09 +0200

    Fixed Controller

--- head/Controller.h
+++ head/Controller.h
@@ -3,25 +3,27 @@
 #include "Game.h"
 #include "Card.h"
 #include "Version.h"
+#include "Board.h"
+#include "Hand.h"
+
 
 
 class Controller{
 private:
 	static Controller* instance;
-  Deck* clan_deck;
-  Deck* tactical_deck;
-  Board* board;
-  Hand* hand_j1;
-  Hand* hand_j2;
+	Deck* clan_deck;
+	Deck* tactical_deck;
+	Board* board;
+	Hand* hand_j1;
+	Hand* hand_j2;
 public:
-	static Controller& getInstance();
-  Deck& getClanDeck();
-  Deck& getTacticalDeck();
-  Board& getBoard();
-  Hand& getHand1();
-  Hand& getHand2();
-	Controller();
-	~Controller();
+	Deck& getClanDeck();
+	Deck& getTacticalDeck();
+	Board& getBoard();
+	Hand& getHand1();
+	Hand& getHand2();
+	Controller() = default;
+	~Controller() = default;
 	struct Handler {
 		Controller* instance = nullptr;
 		~Handler() { delete instance; }
@@ -29,7 +31,6 @@ public:
 	static Handler handler;
 	Controller(const Controller& j) = delete;
 	Controller& operator=(const Controller& j) = delete;
-public:
 	static const Controller& getInstance();
 	void freeInstance();
 };
