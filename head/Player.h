08e5c041a685ca68d7398c02506eab6c16cfcc57
OrianeLanfranchi <131607571+OrianeLanfranchi@users.noreply.github.com>
2023-05-10 22:33:22 +0200

    Update Player.h

--- head/Player.h
+++ head/Player.h
@@ -3,15 +3,34 @@
 #include "Hand.h"
 
 class Player {
+private:
+    std::string name;
+    unsigned int id;
+    unsigned int score = 0;
+    Hand* hand = nullptr;
+    friend class Controller;
 public:
-  Player(const string& n, bool i, bool ia){};
-    + fonctionIA() 
-    + getNom() const : const string&
-    + getId() const : unsigned int
-    + estIA() const : bool;
-    + getScore() const : unsigned int
-    + getMain() const : const Cartes**
+    const string& getName() const { return name; }
+    unsigned int getId() const { return id; }
+    unsigned int getScore() const { return score; }
+    const Hand* getHand() const { return hand; }
+    void setHand(const Card* c[6]) {
+        if (hand == nullptr) hand = new Hand(c);
+    }
+    void setHand(const Card** c, size_t n) {
+        if (hand == nullptr) hand = new Hand(c, n);
+    }
+protected :
+    Player(const string& n, bool idPlayer)
+        : name(n), id(idPlayer) {}
+    ~Player() {
+        delete hand;
+    }
+};
 
-}class PlayerAIRandom : public Player {
+class PlayerAIRandom : public Player {
+private:
+    //
+public:
     PlayerAIRandom(const string& n, bool idPlayer) : Player(n, idPlayer) {}
 };
