2d62678eb5e02c30e41cadd6f3b03e549e4abea3
WAnoukh <anoukhman@gmail.com>
2023-05-11 00:32:12 +0200

    Code Adapted to card changes + renaming + WIP border revendication code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 #include "head/Game.h"
+#include "head/Card.h"
+#include "head/Board.h"
 
 int main() {
-  std::cout << "Hello World!\n";
-  std::cout << "Hello there!\n";
-  std::cout << "Anoukhan Skywalker\n";
-  std::cout << "coucou";
+	const Card** cards = new const Card*[10];
+
+	cards[0] = new Clan(Color::blue, Number::nine);
+	cards[1] = new Clan(Color::blue, Number::seven);
+	cards[2] = new Clan(Color::blue, Number::eight);
+
+
+	const Card*  icomb[] {new Clan(Color::blue,Number::one),new Clan(Color::blue,Number::two) };
+
+	const Card** bestVar = Stone::bestVariation(cards, 3, icomb, 2, 4);
+
+
+	if (bestVar != nullptr) {
+		for (int i = 0; i < 4; i++) {
+			if (bestVar[i] != nullptr) {
+				cout << bestVar[i]->getName() << endl;
+			}
+			else {
+				cout << "nullptr" << endl;
+			}
+
+		}
+	}
+	else {
+		cout << "nothing found !";
+	}
+
 }
 
 
