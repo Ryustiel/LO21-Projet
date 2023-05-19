9791b221c3daf38c98eecdf994da8bd3a7b9fd6c
OrianeLanfranchi <131607571+OrianeLanfranchi@users.noreply.github.com>
2023-05-19 15:18:25 +0200

    + Ui

---
+++ head/UserInterface.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Game.h"
+#include "Supervisor.h"
+#include "Card.h"
+#include "Board.h"
+#include "Version.h"
--- main.cpp
+++ main.cpp
@@ -9,32 +9,35 @@
 #include "head/Version.h"
 
 int main() {
-
 	int selection;
 	Version selected_version;
 	string p1_name;
 	string p2_name;
-
+	int i = 0;
+	
 	cout << "***  SCHOTTEN TOTTEN  ***" << endl << endl;
 	cout << "Creating a supervisor..." << endl;
 
 	Supervisor::getInstance();
 
 	cout << "Supervisor created." << endl;
-	//system("pause");
-	//cout << system("CLS") << endl;
+
+	system("pause");
+	system("CLS");
 
 	cout << "***  SCHOTTEN TOTTEN  ***" << endl << endl;
 	cout << "** VERSION MENU **" << endl;
 	cout << "Available versions :" << endl;
+
+	i = 0;
 	for (auto& v : Versions) {
-		cout << v << endl;
+		cout << i++ << " : " << v << endl;
 	}
-	cout << endl << "Please select a version :" << endl;
+	cout << endl << "Please select a version (number) :" << endl;
 
 	cin >> selection;
 
-	int i = 0;
+	i = 0;
 	for (auto& v : Versions) {
 		if (i == selection) {
 			i = -1;
@@ -44,14 +47,15 @@ int main() {
 		i++;
 	}
 	if (i != -1) { //no version selected
+		cout << "Incorrect selected version. Exitting the process...";
 		exit(1);
 	 }
 
 	cout << "Selected version : " << selected_version << endl;
 
-	//system("pause");
+	system("pause");
+	system("CLS");
 
-	//cout << system("CLS") << endl;
 	cout << "***  SCHOTTEN TOTTEN  ***" << endl << endl;
 	cout << "** SETTINGS MENU **" << endl;
 	cout << "Player 1 name :" << endl;
@@ -61,6 +65,7 @@ int main() {
 
 	cout << "Creating controller..." << endl;
 	Supervisor::getInstance().setController(selected_version, p1_name, p2_name, 1, 2);
-	cout << Supervisor::getInstance().getController()->getPlayer1()->getName();
+	cout << Supervisor::getInstance().getController()->getPlayer1()->getName() << endl;
+	cout << Supervisor::getInstance().getController()->getPlayer2()->getName() << endl;
 	cout << "end";
 }
