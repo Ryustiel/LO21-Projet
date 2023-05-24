c5886efa65a17effc5aa9ee0932069e61ec71315
OrianeLanfranchi <131607571+OrianeLanfranchi@users.noreply.github.com>
2023-05-24 14:30:20 +0200

    fixes version

--- head/Version.h
+++ head/Version.h
@@ -1,10 +1,15 @@
 #pragma once
+#include "../exception/ShottenTottenException.h"
+#include <string>
+#include <iostream>
 
-enum class Version { legacy, tactic};
-
+using namespace std;
+enum class Version { legacy, tactic };
+extern std::initializer_list<Version> Versions;
 
 extern Version current_version;
 const Version& getVersion();
 void setVersion(const Version& v);
-
+string toString(const Version& v);
+std::ostream& operator<<(std::ostream& f, const Version& v);
 
