f4567e343c8fa64ab5bd4a67cd952c650278f949
cam-jo <cam.cjo21@gmail.com>
2023-05-27 12:46:06 +0200

    essai commit new branch

--- interface/src_view/main.cpp
+++ interface/src_view/main.cpp
@@ -1,11 +1,15 @@
 #include <QApplication>
 #include "../head_view/parametersview.h"
+#include "../head_view/boardview.h"
+
 
 int main(int argc, char* argv[]) {
 
     QApplication app(argc, argv);
     VueVersion version;
-    version.show();
+    VuePartie partie;
+    partie.show();
+    //version.show();
     return app.exec();
 }
 
